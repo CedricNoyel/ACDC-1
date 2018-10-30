8d71e1366ca0455c0ffd881c241e4d24de855276
Raphael Hascoet <hascoetraphael-29@yahoo.fr>
2018-10-30 22:25:49 +0100

    Implemented URL and Image mardown generators

--- src/post/MarkdownGenerator.java
+++ src/post/MarkdownGenerator.java
@@ -6,4 +6,11 @@ public class MarkdownGenerator {
 		return "";
 	}
 	
+	public static String mdUrl(String text, String url){
+		return String.format("[%s](%s)", text, url); 
+	}
+	
+	public static String mdImage(String urlImg){
+		return String.format("![%s]", urlImg);
+	}
 }
